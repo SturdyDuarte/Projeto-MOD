d08a6f80006a6e17cde1cb22b2146058e9f938a5
SturdyDuarte <112146413+SturdyDuarte@users.noreply.github.com>
2025-08-27 11:45:05 -0300

    Part 1: Collecting and displaying the candidates

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // The beginning of MOD project!!
 const entrada=require('prompt-sync')({ sigint: false });
+let papas = [];
+
+// Collecting names of 5 candidates (Coletando dados de 5 candidatos)
+for (let i = 1; i <= 5; i++){
+    let nome = entrada(`Digite o nome do ${i}° condidato: `);
+    papas.push(nome);
+}
+
+// Displaying the list of candidates (Mostrando a lista de candidatos)
+console.log(papas);
